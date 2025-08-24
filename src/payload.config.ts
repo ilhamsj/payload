ac53807c6c349c4533ba1e769c79ed2ddd1799e2
ilhamsj <ilhamsaputrajati@gmail.com>
2025-08-24 09:20:34 +0700

    feat: add SSL certificate handling for PostgreSQL connection
    - Imported 'fs' module to read the SSL certificate from the file system.
    - Updated database configuration to include SSL settings with the certificate for secure connections.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,12 +6,18 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import fs from 'fs'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+/**
+ * Read the certificate from the file system
+ */
+const certificate = fs.readFileSync(process.env.POSTGRES_SSL_CA_PATH!, 'utf8')
 
 export default buildConfig({
   admin: {
@@ -29,6 +35,10 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
+      ssl: {
+        rejectUnauthorized: false,
+        ca: certificate,
+      },
     },
   }),
   sharp,
